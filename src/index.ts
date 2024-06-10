8b29249af69634d95a80b2909bb3835f44c7c564
Amanvir <amanvir.singh880@gmail.com>
2024-06-10 19:41:12 -0400

    Completed the Prisma Routes

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,13 @@ app.use(express.json());
 app.post(`/post`, async (req, res) => {
   //create a new post and associate it with an author
   const { title, content, authorEmail } = req.body;
-  const result = await prisma;
+  const result = await prisma.post.create({
+    data: {
+      title,
+      content,
+      author: { connect: { email: authorEmail } },
+    },
+  });
   res.json(result);
 });
 
@@ -17,7 +23,14 @@ app.put("/post/:id/views", async (req, res) => {
   const { id } = req.params;
   //update the view count field for a specific post
   try {
-    const post = await prisma;
+    const post = await prisma.post.update({
+      where: {
+        id: Number(id)
+      },
+      data: {
+        viewCount: {increment:1}
+      }
+    });
 
     res.json(post);
   } catch (error) {
@@ -27,10 +40,27 @@ app.put("/post/:id/views", async (req, res) => {
 
 app.put("/publish/:id", async (req, res) => {
   const { id } = req.params;
-
+  let newValue;
   try {
     // toggle the `published` field on the specified post
-    const updatedPost = await prisma;
+    const post = await prisma.post.findFirst({
+      where:{
+        id: Number(id)
+      }
+    })
+    if (post?.published === false) {
+      newValue = true;
+    } else {
+      newValue = false;
+    }
+    const updatedPost = await prisma.post.update({
+      where: {
+        id: Number(id)
+      },
+      data: {
+        published: newValue,
+      }
+    });
 
     res.json(updatedPost);
   } catch (error) {
@@ -41,27 +71,48 @@ app.put("/publish/:id", async (req, res) => {
 app.delete(`/post/:id`, async (req, res) => {
   //delete the post
   const { id } = req.params;
-  const post = await prisma;
+  const post = await prisma.post.delete({
+    where: {
+      id: Number(id)
+    }
+  });
   res.json(post);
 });
 
 app.get("/users", async (req, res) => {
   //return all the users
-  const users = await prisma;
+  const users = await prisma.user.findMany();
   res.json(users);
 });
 
 app.get("/user/:id/drafts", async (req, res) => {
   const { id } = req.params;
   //return all posts where the published field equals false
-  const drafts = await prisma;
+  const drafts = await prisma.post.findMany({
+    where: {
+      AND: [
+        {
+          authorId : Number(id)
+        },
+        {
+          published: false
+        }
+      ]
+    }
+  });
   res.json(drafts);
 });
 
 app.get(`/post/:id`, async (req, res) => {
   const { id }: { id?: string } = req.params;
   //return the post
-  const post = await prisma;
+  const post = await prisma.post.findFirst(
+    {
+      where: {
+        id: Number(id)
+      }
+    }
+  );
   res.json(post);
 });
 
@@ -74,7 +125,21 @@ app.get("/feed", async (req, res) => {
   // 5. order the posts by the field `updated_at` descending or ascending basesd on the parameter `orderBy`
   // 6. if the `searchString` parameter is not an empty, use the string to filter posts not matching the post titles or post content
 
-  const posts = await prisma.post.findMany();
+  const posts = await prisma.post.findMany({
+    where: {
+      published: true,
+      OR: [
+        {title: { contains: searchString as string }},
+        {content: { contains: searchString as string }}
+      ]
+    },
+    include: { author: true },
+    skip: skip ? Number(skip) : undefined,
+    take: take ? Number(take) : undefined,
+    orderBy: {
+      updatedAt: orderBy === "asc"? "asc" : "desc"
+    }
+  });
 
   res.json(posts);
 });
